9bcf8792c5cb06d53963841e92e5c4d06cadd5f5
TahirMansuri <tprocks786@gmail.com>
2021-11-28 12:37:32 +0530

    MODIFIED to Store Result in Dictionary

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+#Creating Dictionary for Results
+declare -A resultDict
+
 echo "Welcome to Sorting of Arithmetic Computation Program";
 
 #Take 3 Input from User
@@ -8,13 +11,18 @@ read -p "Enter the Second Number: " b;
 read -p "Enter the Third Number: " c;
 
 #Computing the Arithmetic 1st Value
-w=$(( $a + $b * $c ));
+w=(( $a + $b * $c ));
+resultDict[compute1]=$w;
 
 #Computing the Arithmetic 2nd Value
 x=$(( $a * $b + $c ));
+resultDict[Compute2]=$x;
 
 #Computing the Arithmetic 3rd Value
 y=$(( $c + $a / $b ));
+resultDict[Compute3]=$y;
 
 #Computing the Arithmetic 4th Value
 z=$(( $a % $b + $c ));
+resultDict[Compute4]=$z;
+
